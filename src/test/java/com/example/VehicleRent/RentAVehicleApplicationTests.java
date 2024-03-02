90e3ffd690329b6432090ffbc5c64fc8fd11c2ea
Selvanigethan <nik261190@gmail.com>
2024-03-02 19:40:16 +0000

    second commit

---
+++ src/test/java/com/example/VehicleRent/RentAVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VehicleRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentAVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
